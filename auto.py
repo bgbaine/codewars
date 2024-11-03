c86b7d1ef0a042dfb50cc494cc9bd0e8907c434b
Bernardo <bgbaine@gmail.com>
2024-11-03 00:25:08 -0300

    feat: add aditional layer of abstraction

--- auto.py
+++ auto.py
@@ -4,7 +4,7 @@ import time
 from helpers import (
     fetchUserData,
     fetchKataData,
-    create_filename,
+    format_filename,
     create_directory,
     create_file,
 )
@@ -23,10 +23,11 @@ def main():
             raise Exception("Failed to fetch kata data from Codewars API")
 
         for language in object["completedLanguages"]:
-            directory: str = (
-                f'{language}/{(kata_response["rank"]["name"]).replace(" ", "-")}/{kata_response["slug"]}'
-            )
-            filename: str = create_filename(kata_response["slug"], language)
+            kata_rank: str = kata_response["rank"]["name"]
+            kata_slug: str = kata_response["slug"]
+
+            directory: str = create_directory(language, kata_rank, kata_slug)
+            filename: str = format_filename(kata_slug, language)
 
             create_directory(directory)
             create_file(directory, filename)
